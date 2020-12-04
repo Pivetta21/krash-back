3f29bfb2d2f7c7eba4366cf51d49b20ae5b64be3
Lucas Pivetta <lucaspivetta21@gmail.com>
2020-12-03 23:45:23 -0300

    Att GET, add PUT/DELETE

--- src/main/java/br/pivetta/krash/controller/PermissionController.java
+++ src/main/java/br/pivetta/krash/controller/PermissionController.java
@@ -27,7 +27,14 @@ public class PermissionController {
 
     @Cacheable(value = "permissionsList")
     @GetMapping
-    public Page<Permission> getPermissions(@PageableDefault(sort = "id", direction = Sort.Direction.ASC, page = 0, size = 10) Pageable pageable) {
+    public Page<Permission> getPermissions(
+            @RequestParam(required = false) String permissionName,
+            @PageableDefault(sort = "id", direction = Sort.Direction.ASC, page = 0, size = 10) Pageable pageable
+    ) {
+        if(permissionName != null) {
+            return permissionRepository.findByName(permissionName, pageable);
+        }
+
         return permissionRepository.findAll(pageable);
     }
 
@@ -51,5 +58,36 @@ public class PermissionController {
         URI uri = uriBuilder.path("/permission/{id}").buildAndExpand(permission.getId()).toUri();
 
         return ResponseEntity.created(uri).body(permission);
+    }
+
+    @CacheEvict(value = "permissionsList", allEntries = true)
+    @Transactional
+    @PutMapping("/{id}")
+    public ResponseEntity<Permission> updatePermission(@PathVariable Long id, @RequestBody @Valid Permission updatedPermission) {
+        Optional<Permission> permissionOptional = permissionRepository.findById(id);
+
+        if (permissionOptional.isPresent()) {
+            Permission permission = permissionOptional.get();
+            permission.setName(updatedPermission.getName());
+
+            return ResponseEntity.ok(permission);
+        }
+
+        return ResponseEntity.notFound().build();
+    }
+
+    @CacheEvict(value = "permissionsList", allEntries = true)
+    @Transactional
+    @DeleteMapping("/{id}")
+    public ResponseEntity<?> dedeletePermission(@PathVariable Long id) {
+        Optional<Permission> permission = permissionRepository.findById(id);
+
+        if (permission.isPresent()) {
+            permissionRepository.deleteById(id);
+
+            return ResponseEntity.ok().build();
+        }
+
+        return ResponseEntity.notFound().build();
     }
 }
--- src/main/java/br/pivetta/krash/repository/PermissionRepository.java
+++ src/main/java/br/pivetta/krash/repository/PermissionRepository.java
@@ -1,10 +1,12 @@
 package br.pivetta.krash.repository;
 
 import br.pivetta.krash.model.Permission;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
 public interface PermissionRepository extends JpaRepository<Permission, Long> {
-
+    Page<Permission>findByName(String permissionName, Pageable pageable);
 }
