23d0b1024d099b7df1e659f13778e9a9fed5220a
Lucas Pivetta <lucaspivetta21@gmail.com>
2020-12-03 23:05:42 -0300

    Implementação do handle de erro para exceções de validação de argumentos

---
+++ src/main/java/br/pivetta/krash/controller/ErrorHandler.java
@@ -0,0 +1,36 @@
+package br.pivetta.krash.controller;
+
+import br.pivetta.krash.model.Error;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.MessageSource;
+import org.springframework.context.i18n.LocaleContextHolder;
+import org.springframework.http.HttpStatus;
+import org.springframework.validation.FieldError;
+import org.springframework.web.bind.MethodArgumentNotValidException;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.ResponseStatus;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@RestControllerAdvice
+public class ErrorHandler {
+    @Autowired
+    MessageSource messageSource;
+
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+    @ResponseStatus(HttpStatus.BAD_REQUEST)
+    public List<Error> handleArgumentNotValidException(MethodArgumentNotValidException exception) {
+        List<Error> errors = new ArrayList<>();
+
+        List<FieldError> fieldErrors = exception.getBindingResult().getFieldErrors();
+        fieldErrors.forEach(error -> {
+            String message = messageSource.getMessage(error, LocaleContextHolder.getLocale());
+            String field = error.getField();
+            errors.add(new Error(field, message));
+        });
+
+        return errors;
+    }
+}
